ff513cdc662ec3c50e3147b3933e9061462c855d
hujiazhen <hujiazhen@autohome.com.cn>
2021-12-30 10:38:57 +0800

    hotCode

--- src/HotCode/Code20.java
+++ src/HotCode/Code20.java
@@ -1,5 +1,6 @@
 package HotCode;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class Code20 {
@@ -8,13 +9,22 @@ public class Code20 {
     }
 
     public boolean isValid(String s) {
+
+        HashMap<Character,Character> pairs=new HashMap<>();
+        pairs.put('{','}');
+        pairs.put('[',']');
+        pairs.put('(',')');
+
         int length=s.length();
         if(length%2!=0){
          return false;
         }else {
             Stack<Character> stack=new Stack<>();
             for (int i = 0; i < length; i++) {
-
+                char ch = s.charAt(i);
+                if(pairs.containsKey(ch)){
+                    
+                }
             }
         }
 
